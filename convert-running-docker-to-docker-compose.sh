65abe4264e9dd51578804318c549e9b6e8245339
Nick Nelson <nicholasadamnelson@gmail.com>
2022-06-15 20:34:48 -0400

    Update convert-running-docker-to-docker-compose.sh

--- convert-running-docker-to-docker-compose.sh
+++ convert-running-docker-to-docker-compose.sh
@@ -1,2 +1,2 @@
 #!
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/red5d/docker-autocompose CONTAINERNAME
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/red5d/docker-autocompose CONTAINERNAME >> CONTAINERNAME.yml
